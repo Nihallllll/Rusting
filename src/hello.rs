6744a4f37f4a4c76b5f73d6e01632872621b896e
Nihallllll <nihal24rajak@gmail.com>
2025-08-04 13:40:52 +0530

    added

--- src/hello.rs
+++ src/hello.rs
@@ -1,24 +1,32 @@
-    struct Triangle {
-        p : u32,
-        b : u32,
-        h : u32
-    }
-    //we have to define the struct before hand to use
-    impl Triangle{
-        fn area(&self){
+    // struct Triangle {
+    //     p : u32,
+    //     b : u32,
+    //     h : u32
+    // }
+    // //we have to define the struct before hand to use
+    // impl Triangle{
+    //     fn area(&self){
             
-            println!("the area is : {}",self.p +self.b +self.h);
-        }
-        fn print(){
-            println!("the area is : {}",121)
-        }
+    //         println!("the area is : {}",self.p +self.b +self.h);
+    //     }
+    //     fn print(){
+    //         println!("the area is : {}",121)
+    //     }
+    // }
+    // pub fn give_pbh(a: u32,b: u32,c: u32){
+    // let triangle = Triangle{
+    //     p: a,
+    //     b: b,
+    //     h: c
+    // };
+    // println!("height : {}, Base : {} , hypotenues : {}",triangle.p,triangle.b,triangle.h);
+    // println!("the area is : {:?}", triangle.area());
+    // }
+
+
+    struct User {
+        name: String,
+        age: u32
     }
-    pub fn give_pbh(a: u32,b: u32,c: u32){
-    let triangle = Triangle{
-        p: a,
-        b: b,
-        h: c
-    };
-    println!("height : {}, Base : {} , hypotenues : {}",triangle.p,triangle.b,triangle.h);
-    println!("the area is : {:?}", triangle.area());
-    }
+
+    
